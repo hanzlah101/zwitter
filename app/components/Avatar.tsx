b0a799262a47f31615025055bdea6ceb003618e5
hanzlah101 <116737045+hanzlah101@users.noreply.github.com>
2023-04-23 16:10:52 +0500

    Update Avatar.tsx

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Image from "next/image";
+
 import { FC } from "react";
 
 interface AvatarProps {
@@ -11,17 +11,14 @@ interface AvatarProps {
 
 const Avatar: FC<AvatarProps> = ({ src, alt = "_profile_image_", large }) => {
   return (
-    <Image
+    <img
       className={`rounded-full object-cover ${
         large
           ? "md:w-36 md:h-36 w-32 h-32 md:border-[5px] border-4 border-white"
           : "w-10 h-10 border-none"
       }`}
-      width={"20"}
-      height={"20"}
       src={src}
       alt={alt}
-      unoptimized
     />
   );
 };
